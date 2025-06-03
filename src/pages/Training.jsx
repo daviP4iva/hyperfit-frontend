cb49375c851ae7028c27736e8370e0e21e478359
mench05 <men.chen01@estudiant.upf.edu>
2025-06-03 19:12:26 +0200

    Change

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -13,9 +13,13 @@ const Training = () => {
   };
 
   const handleGenerateRoutine = () => {
-    // In a real app, you would call an AI service to generate a routine
-    // For now, we'll just navigate back to the dashboard
-    navigate('/generated-routine');
+    navigate('/generated-routine', {
+      state: {
+        goal,
+        level,
+        daysPerWeek
+      }
+    });
   };
 
   return (
@@ -135,7 +139,7 @@ const Training = () => {
               <span>Inicio</span>
             </Link>
             
-            <Link to="/coming-soon" className="nav-item">
+            <Link to="/search" className="nav-item">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M21 21L16.65 16.65" stroke="#333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
